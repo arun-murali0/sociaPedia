befbfa73cdfd94e1f0b8a33366f3b6ae469b6ff6
arun-murali0 <arunkarthick0810@gmail.com>
2024-07-30 13:42:50 +0530

    login-method created

--- server/src/controller/auth/createNewUser.ts
+++ server/src/controller/auth/createNewUser.ts
@@ -1,8 +1,12 @@
 import { Request, Response } from 'express';
 import { tryCatch } from '../../middleware/tryCatch';
+import { User } from '../../models/user';
 
 export const createNewUser = tryCatch(async (req: Request, res: Response) => {
   const userData = req.body;
-
-  
+  const user = await User.create(userData);
+  if (user.email) {
+    return res.status(400).json({ error: 'already Regsitered' });
+  }
+  res.status(200).json({ message: 'User created successfully' });
 });
--- server/src/controller/auth/userLogin.ts
+++ server/src/controller/auth/userLogin.ts
@@ -1,11 +1,12 @@
 import { Request, Response } from 'express';
 import { tryCatch } from '../../middleware/tryCatch';
+import { User } from '../../models/user';
 
 export const userLogin = tryCatch(async (req: Request, res: Response) => {
-  const userData = req.body;
-
-
-
-
-  
+  const { email, password } = req.body;
+  const user = await User.find({ email: email }).exec();
+  if (!user) {
+    return res.status(200).json({ message: 'user not Found' });
+  }
+  res.status(200).json({ message: 'Login successfull' });
 });
