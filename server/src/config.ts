2fdabd59da9d615efdd6b4d0e6b20659e478a73b
arun-murali0 <arunkarthick0810@gmail.com>
2024-07-30 16:48:29 +0530

    some types alias change for data

--- server/src/config.ts
+++ server/src/config.ts
@@ -3,6 +3,6 @@ dotenv.config();
 
 const PORT = process.env.PORT || 3030;
 const MONGO_STRING = process.env.MONGO_URL;
-const SECRET = process.env.SECRET || undefined;
+const SECRET = process.env.SECRET;
 
 export { PORT, MONGO_STRING, SECRET };
--- server/src/routes/auth-route/index.ts
+++ server/src/routes/auth-route/index.ts
@@ -1,6 +1,8 @@
 import { Router } from 'express';
 import { createNewUser, userLogin } from '../../controller';
 import passport from 'passport';
+
+// email and password startegy
 import '../../passport-strategy/local-strategy';
 
 const router = Router();
@@ -8,6 +10,7 @@ const router = Router();
 // new user creation routes
 router.post('/create', createNewUser);
 
+// login
 router.post('/login', passport.authenticate('local'), userLogin);
 
 export default router;
