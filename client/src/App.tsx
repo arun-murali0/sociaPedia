e615cb73ec7169012f3b0ad42ae5ef7ddc240388
arun-murali0 <arunkarthick0810@gmail.com>
2024-07-31 12:01:20 +0530

    tailwind-theme-setup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,9 @@
+import Home from './view/Home';
+
 export default function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <Home />
+    </div>
+  );
 }
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,33 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    colors: {
+      black: '#383d3b',
+      white: '#ffffff',
+      grey: '#f7f7f7',
+      purple: '#b084cc',
+      green: '#44cf6c',
+    },
+    fontSize: {
+      smallFont: '14px',
+      normalFont: '16px',
+      largerFont: '18px',
+    },
+    extend: {
+      spacing: {
+        128: '32rem',
+        144: '36rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      },
+    },
   },
   plugins: [],
 };
